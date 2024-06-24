cdf4094649ae504001d2838d76c0691c71b35e23
omer2080 <118855264+omer2080@users.noreply.github.com>
2024-06-24 16:50:17 +0300

    Fixing Pricing:
    Converting pricing values from floats to integers.

--- flight_search.py
+++ flight_search.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 
 from flight_data import FlightData
-from distance_calculator import DistanceCalculator
 
 load_dotenv()
 
@@ -23,19 +22,10 @@ class FlightSearch:
         code = results[0]["code"]
         return code
 
-    def check_flights(self, origin_iata_code, destination_city, destination_country, destination_iata_code, min_nights, max_nights, num_of_cheapests_flights_per_city, fly_date_from, fly_date_to, stopovers, currency, max_price_to_pay):
-        
-        distance_calculator = DistanceCalculator()
-        destination_location = f"{destination_city}, {destination_country}"
-        distance = distance_calculator.calculate_distance("Tel Aviv", destination_location)
-        # print(f'\nThe distance from {destination_city} to Tel Aviv is {distance:.2f} kilometers.\n')
-        
-        if distance > 4500:
+    def check_flights(self, origin_iata_code, destination_iata_code, min_nights, max_nights, num_of_cheapests_flights_per_city, fly_date_from, fly_date_to, stopovers, currency, max_price_to_pay):
+        if destination_iata_code == 'BKK' or destination_iata_code == 'SYD':
             min_nights = 10
             max_nights = 20
-            stopovers = 2
-        #else staying with default settings
-        
         headers = {"apikey": my_api}
         query = {
             "fly_from": origin_iata_code,
@@ -66,11 +56,10 @@ class FlightSearch:
         flight_data = FlightData(
             price = flight_data_list["price"],
             origin_city = "Tel Aviv",
-            destination_city = flight_data_list["cityTo"],
-            destination_airport = flight_data_list["flyTo"],
+            destination_city = flight_data_list["route"][0]["cityTo"],
+            destination_airport = flight_data_list["route"][0]["flyTo"],
             depart_date = flight_data_list["route"][0]["local_departure"].split("T")[0],
-            return_date = flight_data_list["route"][max(1, len(flight_data_list["route"]) - 1)]["local_departure"].split("T")[0], #the maximum is for flights with connections
-            flight_link = flight_data_list["booking_token"]
+            return_date = flight_data_list["route"][1]["local_departure"].split("T")[0]
         )
         print(f"{flight_data.destination_city}: ${flight_data.price}")
         return flight_data
